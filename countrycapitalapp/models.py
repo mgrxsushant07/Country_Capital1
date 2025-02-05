09d4d9c785760b239f00c0e778a172ab989098ac
Thulo Technology <thulotechnology@gmail.com>
2025-02-05 19:18:43 +0545

    Before Admin

--- countrycapitalapp/models.py
+++ countrycapitalapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=150)
+    capital = models.CharField(max_length=150)
+    population = models.IntegerField()
+    official_language = models.CharField(max_length=50)
+    currency = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
